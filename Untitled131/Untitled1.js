3c8e7f0dc99ef84bba630ed45adff51d303dd52b
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-04-04 17:30:57 +0200

    Initial commit

---
+++ Untitled131/Untitled1.js
@@ -0,0 +1,11 @@
+﻿var envVars = {};
+var resultLines = '';
+var sw = new SystemWorker("wmd", "/C set", null, null, null);
+sw.onmessage = function ( e ) {
+    resultLines += arguments[0].data.toString();
+};
+sw.onerror= function ( e ) {
+    resultLines += arguments[0].data.toString();
+};
+wait(1000);
+resultLines;
