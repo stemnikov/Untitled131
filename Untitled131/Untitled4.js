4339a300806de93bbbfde134b8952faac2d4e3d0
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-04-25 15:59:01 +0200

    Update #33

---
+++ Untitled131/Untitled4.js
@@ -0,0 +1,14 @@
+﻿var envVars = {};
+var resultLines = '';
+var sw = new SystemWorker("cmd /C set", { parameters : [ ] });
+sw.onmessage = function ( e ) {
+    resultLines += arguments[0].data.toString();
+};
+sw.onerror= function ( e ) {
+    resultLines += arguments[0].data.toString();
+};
+wait(1000);
+
+resultLines;
+
+
